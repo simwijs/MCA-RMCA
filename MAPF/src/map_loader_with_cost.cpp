45257debbe468d960b7729dcb14182e616b54263
Simon Wijk Stranius <simon@simonws.se>
2022-02-10 09:40:31 +0100

    Read map row/col by space instead of csv

--- MAPF/src/map_loader_with_cost.cpp
+++ MAPF/src/map_loader_with_cost.cpp
@@ -8,26 +8,23 @@
 #include <fstream>
 #include <boost/tokenizer.hpp>
 
-
 void MapLoaderCost::loadKiva(string fname)
 {
     string line;
-    ifstream myfile (fname.c_str());
+    ifstream myfile(fname.c_str());
     if (!myfile.is_open())
     {
         cerr << "Map file not found." << endl;
         return;
     }
-    //read file
-    getline(myfile, line);
-    boost::char_separator<char> sep(",");
-    boost::tokenizer< boost::char_separator<char> > tok(line, sep);
-    boost::tokenizer< boost::char_separator<char> >::iterator beg = tok.begin();
-    rows = atoi((*beg).c_str()) + 2; // read number of rows
-    beg++;
-    cols = atoi((*beg).c_str()) + 2; // read number of cols
-
+    // read file
     stringstream ss;
+    // Get row and col
+    getline(myfile, line);
+    ss << line;
+    ss >> rows;
+    ss >> cols;
+
     getline(myfile, line);
     ss << line;
     ss >> workpoint_num;
@@ -43,39 +40,37 @@ void MapLoaderCost::loadKiva(string fname)
     ss << line;
     ss >> maxtime;
 
-    bool* my_map= new bool[rows*cols];
-    //DeliverGoal.resize(row*col, false);
-    // read map
+    bool *my_map = new bool[rows * cols];
+    // DeliverGoal.resize(row*col, false);
+    //  read map
     int ep = 0, ag = 0;
-    for (int i = 1; i<rows - 1; i++)
+    for (int i = 1; i < rows - 1; i++)
     {
         getline(myfile, line);
-        for (int j = 1; j<cols - 1; j++)
+        for (int j = 1; j < cols - 1; j++)
         {
-            my_map[cols*i + j] = (line[j - 1] == '@'); // not a block
+            my_map[cols * i + j] = (line[j - 1] == '@'); // not a block
 
-            if (line[j - 1] == 'e') //endpoint
+            if (line[j - 1] == 'e') // endpoint
             {
-                endpoints.push_back( i*cols+ j);
-                assert(ep == (endpoints.size()-1));
+                endpoints.push_back(i * cols + j);
+                assert(ep == (endpoints.size() - 1));
                 ep++;
-
             }
         }
-
     }
     myfile.close();
 
-    //set the border of the map blocked
+    // set the border of the map blocked
     for (int i = 0; i < rows; i++)
     {
-        my_map[i*cols] = false;
-        my_map[i*cols + cols - 1] = false;
+        my_map[i * cols] = false;
+        my_map[i * cols + cols - 1] = false;
     }
     for (int j = 1; j < cols - 1; j++)
     {
         my_map[j] = false;
-        my_map[rows*cols - cols + j] = false;
+        my_map[rows * cols - cols + j] = false;
     }
     this->my_map = my_map;
     // Possible moves [WAIT, NORTH, EAST, SOUTH, WEST]
@@ -89,32 +84,35 @@ void MapLoaderCost::loadKiva(string fname)
     this->initializeKivaMapCost();
 }
 
-int MapLoaderCost::getDistance(int initial, int target, int heading) {
-    assert(target<cost_map.size());
-    if(cost_map[target].empty()){
-        setCostMap(target,heading);
+int MapLoaderCost::getDistance(int initial, int target, int heading)
+{
+    assert(target < cost_map.size());
+    if (cost_map[target].empty())
+    {
+        setCostMap(target, heading);
     }
     assert(initial < cost_map[target].size());
     return cost_map[target][initial].get_hval(heading);
 }
 
-void MapLoaderCost::initializeKivaMapCost(){
-    cost_map.resize(rows*cols);
-    for (int e : endpoints){
+void MapLoaderCost::initializeKivaMapCost()
+{
+    cost_map.resize(rows * cols);
+    for (int e : endpoints)
+    {
         setCostMap(e);
     }
 }
 
-void MapLoaderCost::setCostMap(int location, int heading){
-    if(cost_map.empty()){
-        cost_map.resize(rows*cols);
+void MapLoaderCost::setCostMap(int location, int heading)
+{
+    if (cost_map.empty())
+    {
+        cost_map.resize(rows * cols);
     }
     ComputeHeuristic<MapLoader> ch(0, location, this, heading);
     assert(cost_map[location].empty());
-    assert(location<cost_map.size());
+    assert(location < cost_map.size());
 
     ch.getHVals(cost_map[location]);
 }
-
-
-
